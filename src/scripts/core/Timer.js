88d11b7b41c99abd50bff6667b8771f1949956d6
Primoz Z <pzagor2@gmail.com>
2017-01-27 09:23:27 +0100

    don't stop at last key if we are draging

--- src/scripts/core/Timer.js
+++ src/scripts/core/Timer.js
@@ -111,7 +111,7 @@ export default class Timer {
     }
 
     // stop if we are at last key
-    if (this.time[0] >= this.endKeyTime) {
+    if (this.is_playing === true && this.time[0] >= this.endKeyTime) {
       this.time[0] = this.endKeyTime;
     }
 

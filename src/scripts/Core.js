66fa1db33b50fe0fa7d6259a16413a0f41750e8e
Primoz Z <pzagor2@gmail.com>
2017-02-20 13:56:04 +0100

    add set current time

--- src/scripts/Core.js
+++ src/scripts/Core.js
@@ -36,6 +36,10 @@ class Core {
     this.timer.removeDurationChangedListener(listener);
   }
 
+  setPlaybackTime(time) {
+    this.timer.seek([time * 1000]);
+  }
+
   onDataUpdated() {
     // find last key in this.data
     var lastKeyTime = 0;
